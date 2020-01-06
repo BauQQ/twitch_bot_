859c52cd96ca2c3d7f9e167d51155c4b466e6b79
Bau <bau@xkay.dk>
2020-01-06 20:38:58 +0100

    Add files via upload

---
+++ Chatbot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chatbot.Common;
+using Chatbot.Common.Objects;
+using Chatbot.Common.Models;
+using Chatbot.Common.Actions;
+
+namespace Chatbot
+{
+    public static class Program
+    {
+        [MTAThread]
+        public static void Main(string[] args)
+        {
+            Main m = new Main();
+            m.Run();
+            Console.Read();
+        }
+    }
+}
